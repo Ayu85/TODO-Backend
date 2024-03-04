ec8a10b6a5684020b98c0e783a127c41b7ca5dd7
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-03-04 19:50:56 +0530

    implementing try catch block

--- controllers/createTodo.js
+++ controllers/createTodo.js
@@ -1,1 +1,9 @@
-const Todo=require('../models/todo') //importing the model to create todo controller
+const Todo = require('../models/todo') //importing the model to create todo controller
+const createTodo = async (req, res) => {
+    try {
+
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
