1424c34050d095337880df2a55ab26d857a22d55
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-03-04 11:29:17 +0530

    env variables feeded into process

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 //this file will contain the code for database / mongodb connection with the server
-const mongoose=require('mongoose')
-require()
-const connectDB=()=>{
+const mongoose = require('mongoose')
+require('dotenv').config()
+const connectDB = () => {
     mongoose.connect(process.env.CONNECTION_STRING)
 }
