c9afaaf3885a8663dcad487a59e31777f29d32ba
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-03-04 20:06:56 +0530

    route created and exported

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -1,2 +1,7 @@
 const express = require('express')
-const route = express.router()
+const route = express.router();
+//import the controller which is used to create a todo item
+const todo = require('../controllers/createTodo')
+router.post('/createtodo', todo)
+
+module.exports = router
