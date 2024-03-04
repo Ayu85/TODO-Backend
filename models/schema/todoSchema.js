2a77358b5d633e885e20df57141331b28752aa21
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-03-04 13:03:57 +0530

    folder structure

---
+++ models/schema/todoSchema.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose"
+const { Schema } = mongoose
+//creating a schema / structure of the todo app  i.e. what properties the todo app will contain
+const todo = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    addedAt: {
+        type: Date,
+        required: true,
+        default: Date.now()
+    }
+})
+module.exports=todo
--- models/todo.js
+++ models/todo.js
@@ -1,22 +1,5 @@
 // const moongoose = require('mongoose')
-import mongoose from "mongoose"
-const { Schema, model } = mongoose
-//creating a schema / structure of the todo app  i.e. what properties the todo app will contain
-const todo = new Schema({
-    title: {
-        type: String,
-        required: true,
-    },
-    description: {
-        type: String,
-        required: true
-    },
-    addedAt: {
-        type: Date,
-        required: true,
-        default: Date.now()
-    }
-})
+
 //making the model named ToDo using todo schema
 const todoModel = new model("ToDo", todo)
 module.exports = todoModel
