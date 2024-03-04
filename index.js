b84d8d01ba5668a60da58694deffe147920da052
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-03-04 11:34:35 +0530

    env file has to be imported before use

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //this file will only contain the logic to import ecpress and start the server on a port
 const express = require('express') //importing express
+require('dotenv').config()
 const app = express() //storing express in a variable
 app.listen(process.env.PORT, () => { //to start the server on 3000 port
-    console.log("server started on 3000 port");
+    console.log(`server started on ${process.env.PORT} port`);
 })
