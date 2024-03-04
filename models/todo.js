fe9f827c9c2100fece0b6a85b9be645896b9ea1b
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-03-04 13:04:39 +0530

    schema in different folder

--- models/todo.js
+++ models/todo.js
@@ -1,5 +1,5 @@
 // const moongoose = require('mongoose')
-
+const todoSchema = require('./schema/todoSchema')
 //making the model named ToDo using todo schema
-const todoModel = new model("ToDo", todo)
+const todoModel = new model("ToDo", todoSchema)
 module.exports = todoModel
