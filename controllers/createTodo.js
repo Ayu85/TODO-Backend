767b0ced699097f6e2f9bb9dfa7b0f41b76f9553
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-03-04 19:56:55 +0530

    response sent

--- controllers/createTodo.js
+++ controllers/createTodo.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const Todo = require('../models/todo') //importing the model to create todo controller
 const createTodo = async (req, res) => {
     try {
@@ -5,6 +6,12 @@ const createTodo = async (req, res) => {
         const { title, description } = req.body;
         //creating a new todo object and insert intro the mongo db database 
         const todoData = await Todo.create({ title, description });
+        //send a success code with response tom user
+        res.status(200).json({
+            success: true,
+            data: response,
+            message: "Task created successfully"
+        })
     }
     catch (error) {
         console.log(error);
