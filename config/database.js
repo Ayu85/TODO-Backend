838dbfad93c9e684c497d574b94e523d9c31fffe
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-03-04 11:30:50 +0530

    connection code done

--- config/database.js
+++ config/database.js
@@ -2,5 +2,9 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 const connectDB = () => {
-    mongoose.connect(process.env.CONNECTION_STRING)
+    mongoose.connect(process.env.CONNECTION_STRING).then(() => {
+        console.log("****Boom...DB Connected****");
+    }).catch((err) => {
+        console.log("Oopss...DB Connrction Failed : ", err);
+    })
 }
