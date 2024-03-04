feff17d0b26b53eb036d3801f1b480e316cf8677
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-03-04 11:28:51 +0530

    env library installed

--- config/database.js
+++ config/database.js
@@ -1,1 +1,6 @@
 //this file will contain the code for database / mongodb connection with the server
+const mongoose=require('mongoose')
+require()
+const connectDB=()=>{
+    mongoose.connect(process.env.CONNECTION_STRING)
+}
