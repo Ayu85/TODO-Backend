5a5d1f67ce4431b94ffedf52181e488acd0ca956
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-03-04 11:21:05 +0530

    server code done

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 //this file will only contain the logic to import ecpress and start the server on a port
 const express = require('express')
+const app = express()
+app.listen(3000, () => {
+    console.log("server started on 3000 port");
+})
