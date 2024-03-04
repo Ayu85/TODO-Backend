dda647b586581e962d4b5368e5f56e573f1ab6a5
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-03-04 19:55:19 +0530

    data fetched and stored in db

--- controllers/createTodo.js
+++ controllers/createTodo.js
@@ -1,7 +1,10 @@
 const Todo = require('../models/todo') //importing the model to create todo controller
 const createTodo = async (req, res) => {
     try {
-
+        //extract the data from request body (the data entered by user on the ui)
+        const { title, description } = req.body;
+        //creating a new todo object and insert intro the mongo db database 
+        const todoData = await Todo.create({ title, description });
     }
     catch (error) {
         console.log(error);
