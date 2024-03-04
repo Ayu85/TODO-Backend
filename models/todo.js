82f55ecd214e53d6131c1dae31079be28cc457a0
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-03-04 13:05:07 +0530

    folder structure

--- models/todo.js
+++ models/todo.js
@@ -1,4 +1,4 @@
-// const moongoose = require('mongoose')
+// importing the created schema to make a model
 const todoSchema = require('./schema/todoSchema')
 //making the model named ToDo using todo schema
 const todoModel = new model("ToDo", todoSchema)
