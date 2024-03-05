b5b542a3265996229a0898b09ab046242a3f45e1
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-03-05 11:12:30 +0530

    routes configured

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 //this file will only contain the logic to import ecpress and start the server on a port
 const express = require('express') //importing express
-const todoRoutes = require('./routes/todoRoutes')
+const connectDB = require('./config/database')
 const app = express() //storing express in a variable
 require('dotenv').config()
 //add the parser to parse the content of body
 app.use(express.json())
-const connectDB = require('./config/database')
+//importing todo todo routes
+const todoRoutes = require('./routes/todoRoutes')
+app.use('/v1/api', todoRoutes)
 app.listen(process.env.PORT, () => { //to start the server on 3000 port
     console.log(`server started on ${process.env.PORT} port`);
 })
